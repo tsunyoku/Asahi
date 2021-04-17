0629d93076ecda155cb63c8ef1b74dd2cc30cd27
Iteki <contact@iteki.pw>
2021-04-17 19:54:28 +0000

    v0.0.4 - fully working logins, just need more packet handling now

--- objects/glob.py
+++ objects/glob.py
@@ -2,3 +2,9 @@ import config # indirect use of config
 
 db: 'AsyncSQLPool' # type hinting
 version: 'Version' # once again, type hinting
+
+# cache some things like bcrypt for speeeeeeeeeeeeeeeeeed hours
+cache = {
+    'bcrypt': {}, # store bcrypt pws for speed hours
+    'user': {} # temporarily used to cache tokens until we have a player object
+}
