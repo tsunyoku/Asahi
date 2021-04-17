a45742a00db33c5ecb8f4377fa7710c13bbcf04f
Iteki <contact@iteki.pw>
2021-04-17 16:34:53 +0000

    version & some other stuff, also using hypercorn for speed & unix socket now

--- ext/config.sample.py
+++ ext/config.sample.py
@@ -6,9 +6,5 @@ mysql = {
     'user': 'tsunyoku'
 }
 
-# port for web server to run on (used for nginx connections)
-# you can really set this to anything that isn't used, it really does not matter as long as you reflect it in your nginx config
-port = 9384
-
 # debug is used to print more info & help to debug if you are experiencing issues with the server
 debug = True
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # external imports (some may require to be installed, install using ext/requirements.txt)
 from quart import Quart, Response, request # web server :blobcowboi:
-from cmyui import AsyncSQLPool, log # import console logger (cleaner than print) and database handler
+from cmyui import AsyncSQLPool, Ansi, Version, log # import console logger (cleaner than print | ansi is for log colours), version handler and database handler
 import pyfiglet
 
 # internal imports
@@ -8,15 +8,31 @@ from objects import glob # glob = global, server-wide objects will be stored her
 
 app = Quart(__name__) # handler for webserver :D
 glob.db = AsyncSQLPool() # define db globally
+glob.version = Version(0, 0, 1) # set Asahi version, mainly for future updater but also for tracking
 
 @app.before_serving
 async def connect() -> None: # ran before server startup, used to do things like connecting to mysql :D | "-> None" to specify we don't want to return anything
-    await glob.db.connect(glob.config.mysql) # connect to db using config :p
+    log(f'==== Asahi v{glob.version} starting ====', Ansi.GREEN)
+    try:
+        await glob.db.connect(glob.config.mysql) # connect to db using config :p
+        if glob.config.debug:
+            log('==== Asahi connected to MySQL ====', Ansi.GREEN)
+    except Exception as error:
+        log(f'==== Asahi failed to connect to MySQL ====\n\n{error}', Ansi.LRED)
+    
 
 @app.route("/", methods=['GET']) # only accept GET requests as POST is for login method, see login method below
-async def root():
+async def root() -> Response: # specify we are going to return Response
     message = f"{pyfiglet.figlet_format('Asahi')}\n\ntsunyoku attempts bancho v2, gone right :sunglasses:"
     return Response(message, mimetype='text/plain')
 
-if __name__ == '__main__':
-    app.run(port=glob.config.port, debug=glob.config.debug) # start web server on port provided & with debug if necessary
+@app.route("/", methods=['POST']) # only accept POST requests, we can assume it is for a login request but we can deny access if not
+async def login():
+    headers = request.headers # request headers, used for things such as user ip and agent
+
+    if 'User-Agent' not in headers or headers['User-Agent'] != 'osu!':
+        # request isn't sent from osu client, return nothing
+        return
+
+    if 'osu-token' not in headers: # sometimes a login request will be a re-connect attempt, in which case they will already have a token, if not: login the user
+        data = await request.data # request data, used to get info such as username to login the user
