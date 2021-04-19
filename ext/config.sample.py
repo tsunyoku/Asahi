f50a7ec1ce016a852634f207c83ad3de4684c12f
Atlas <68862510+atteure@users.noreply.github.com>
2021-04-19 14:49:21 +0100

    Update config.sample.py

--- ext/config.sample.py
+++ ext/config.sample.py
@@ -1,6 +1,3 @@
-domain = 'tsunyoku.xyz'
-menu_image = 'https://a.iteki.pw/1' # change to the link to an image, must be jpg/jpeg, png or jfif 
-
 # mysql auth info
 mysql = {
     'db': 'asahi',
@@ -11,3 +8,5 @@ mysql = {
 
 # debug is used to print more info & help to debug if you are experiencing issues with the server
 debug = True
+domain = 'tsunyoku.xyz'
+menu_image = 'https://a.iteki.pw/1' # change link to image you'd like to display on the main menu; must be jpg/jpeg, png or jfif 
