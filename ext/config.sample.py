dff7ceafeb059e8df048564bc1d0809a1f6041a0
James <51536154+tsunyoku@users.noreply.github.com>
2021-04-19 17:22:18 +0100

    Update config.sample.py

--- ext/config.sample.py
+++ ext/config.sample.py
@@ -6,7 +6,7 @@ mysql = {
     'user': 'tsunyoku'
 }
 
-# debug is used to print more info & help to debug if you are experiencing issues with the server
-debug = True
-domain = 'tsunyoku.xyz'
+debug = True # debug is used to print more info & help to debug if you are experiencing issues with the server
+
+domain = 'tsunyoku.xyz' # domain callback when clicking ingame menu image
 menu_image = 'https://a.iteki.pw/1' # change link to image you'd like to display on the main menu; must be jpg/jpeg, png or jfif 
