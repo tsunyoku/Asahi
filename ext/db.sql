9b44a6d0470dec24d6848511af12f0b921d3ce5c
Iteki <contact@iteki.pw>
2021-04-22 21:06:21 +0000

    update db structure

--- ext/db.sql
+++ ext/db.sql
@@ -84,6 +84,66 @@ ALTER TABLE `friends`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Table structure for table `stats`
+--
+
+CREATE TABLE `stats` (
+  `id` int(11) NOT NULL,
+  `rscore_std` int(255) NOT NULL DEFAULT '0',
+  `acc_std` float(6,2) NOT NULL DEFAULT '0.00',
+  `pc_std` int(255) NOT NULL DEFAULT '0',
+  `tscore_std` int(255) NOT NULL DEFAULT '0',
+  `rank_std` int(255) NOT NULL DEFAULT '0',
+  `pp_std` int(255) NOT NULL DEFAULT '0',
+  `rscore_mania` int(255) NOT NULL DEFAULT '0',
+  `acc_mania` float(6,2) NOT NULL DEFAULT '0.00',
+  `pc_mania` int(255) NOT NULL DEFAULT '0',
+  `tscore_mania` int(255) NOT NULL DEFAULT '0',
+  `rank_mania` int(255) NOT NULL DEFAULT '0',
+  `rscore_catch` int(255) NOT NULL DEFAULT '0',
+  `acc_catch` float(6,2) NOT NULL DEFAULT '0.00',
+  `pc_catch` int(255) NOT NULL DEFAULT '0',
+  `tscore_catch` int(255) NOT NULL DEFAULT '0',
+  `rank_catch` int(255) NOT NULL DEFAULT '0',
+  `rscore_taiko` int(255) NOT NULL DEFAULT '0',
+  `acc_taiko` float(6,2) NOT NULL DEFAULT '0.00',
+  `pc_taiko` int(255) NOT NULL DEFAULT '0',
+  `tscore_taiko` int(255) NOT NULL DEFAULT '0',
+  `rank_taiko` int(255) NOT NULL DEFAULT '0',
+  `pp_taiko` int(255) NOT NULL DEFAULT '0',
+  `pp_catch` int(255) NOT NULL DEFAULT '0',
+  `pp_mania` int(255) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stats`
+--
+
+INSERT INTO `stats` (`id`, `rscore_std`, `acc_std`, `pc_std`, `tscore_std`, `rank_std`, `pp_std`, `rscore_mania`, `acc_mania`, `pc_mania`, `tscore_mania`, `rank_mania`, `rscore_catch`, `acc_catch`, `pc_catch`, `tscore_catch`, `rank_catch`, `rscore_taiko`, `acc_taiko`, `pc_taiko`, `tscore_taiko`, `rank_taiko`, `pp_taiko`, `pp_catch`, `pp_mania`) VALUES
+(1, 0, 0.00, 0, 0, 0, 0, 0, 0.00, 0, 0, 0, 0, 0.00, 0, 0, 0, 0, 0.00, 0, 0, 0, 0, 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stats`
+--
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stats`
+--
+ALTER TABLE `stats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
