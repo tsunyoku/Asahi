6167b02038aa39d82408af8aa5ae37f5b6a86ba8
Iteki <contact@iteki.pw>
2021-06-16 21:15:24 +0000

    thats all for tonight

--- disc/bot.py
+++ disc/bot.py
@@ -11,7 +11,13 @@ class asahiBot(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         log('==== Asahi Discord bot started ====', Ansi.GREEN)
-        await self.bot.change_presence(status=Status.dnd, activity=Activity(type=ActivityType.watching, name=f'over {len(glob.players) - 1} online user(s)'))
+        if (len(glob.players) - 1) > 0:
+            activity = Activity(type=ActivityType.watching, name=f'over {len(glob.players) - 1} online user(s)')
+        else:
+            total_users = await glob.db.fetchval('SELECT COUNT(*) FROM users')
+            activity = Activity(type=ActivityType.watching, name=f'over {total_users} total users')
+
+        await self.bot.change_presence(status=Status.dnd, activity=activity)
 
 def setup(bot):
     bot.add_cog(asahiBot(bot))
