4f097c8266cd3c611336e0671196b9216d8fe847
Iteki <contact@iteki.pw>
2021-06-17 15:21:48 +0000

    cool

--- disc/bot.py
+++ disc/bot.py
@@ -1,6 +1,7 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import Status, ActivityType, Activity
 from cmyui import log, Ansi
+from random import choice
 
 from objects import glob
 
@@ -8,16 +9,24 @@ class asahiBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        log('==== Asahi Discord bot started ====', Ansi.GREEN)
-        if (len(glob.players) - 1) > 0:
-            activity = Activity(type=ActivityType.watching, name=f'over {len(glob.players) - 1} online user(s)')
-        else:
-            total_users = await glob.db.fetchval('SELECT COUNT(*) FROM users')
-            activity = Activity(type=ActivityType.watching, name=f'over {total_users} total users')
+    @tasks.loop(minutes=1)
+    async def set_status(self):
+        statuses = [
+            {'type': ActivityType.watching, 'message': f'over {len(glob.players) - 1} online user(s)'},
+            {'type': ActivityType.playing, 'message': "with Asahi's code"}
+        ]
+
+        status = choice(statuses)
+        activity = Activity(type=status['type'], name=status['message'])
 
         await self.bot.change_presence(status=Status.dnd, activity=activity)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if glob.config.debug:
+            log('==== Asahi Discord bot started ====', Ansi.GREEN)
+        
+        await self.set_status.start()
 
 def setup(bot):
     bot.add_cog(asahiBot(bot))
