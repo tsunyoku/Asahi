fb27cc09870835ea8bfb49e2bacbde6cb190a606
Aochi <36964527+7ez@users.noreply.github.com>
2022-01-06 13:36:20 +0100

    oh fuck off deepsource

--- constants/commands.py
+++ constants/commands.py
@@ -779,9 +779,8 @@ async def process(user: Player, msg: str, public: bool = False) -> Optional[str]
                 return
 
             ret = await c["cb"](user, args[1:])
-            if c["elapsed"]:
-                if ret != None:
-                    ret += f" | Time Elapsed: {(time.time() - start) * 1000:.2f}ms"
+            if c["elapsed"] and ret is not None:
+                ret += f" | Time Elapsed: {(time.time() - start) * 1000:.2f}ms"
 
             return ret or None
     else:
